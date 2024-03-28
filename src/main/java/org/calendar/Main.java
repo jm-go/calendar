57ea5ea08577ba97a53a6a80c88764b53ea5c5f1
Jowita Gogol <jmg88@bath.ac.uk>
2024-03-28 14:21:12 +0000

    Added method to parse time to minutes from midnight

--- src/main/java/org/calendar/Main.java
+++ src/main/java/org/calendar/Main.java
@@ -1,7 +1,23 @@
 package org.calendar;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+       //test method here
+    }
+
+    public List<String[]> seeTimeBlocks (String[] calendar1, String[] dailyBounds1, String[] calendar2, String[] dailyBounds2, int meetingDuration) {
+        //TODO: everything
+        return null;
+    }
+
+    /* Convert time strings into minutes since midnight. */
+    private int parseTimeToMinutes(String time) {
+        String[] timeParts = time.split(":");
+        int hours = Integer.parseInt(timeParts[0]);
+        int minutes = Integer.parseInt(timeParts[1]);
+        return hours * 60 + minutes;
     }
 }
